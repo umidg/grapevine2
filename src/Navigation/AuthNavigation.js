de5dd6e5a7d2bea1b09d35c59dc3386bf1d2f23d
Umid <umidghimire11@gmail.com>
2022-06-13 22:32:17 +0545

    user signup

--- src/Navigation/AuthNavigation.js
+++ src/Navigation/AuthNavigation.js
@@ -1,15 +1,24 @@
-import React, { useContext, useState } from "react";
-import SignedInStack from "./SignedInStack";
-import SignedOutStack from "./SignedOutStack";
-import { UserValue } from "../Context/UserContext";
-import Splash from "../Pages/Splash";
+import React, { useContext, useState } from 'react';
+import SignedInStack from './SignedInStack';
+import SignedOutStack from './SignedOutStack';
+import { UserValue } from '../Context/UserContext';
+import Splash from '../Pages/Splash';
+import { Box } from 'native-base';
 const AuthNavigation = () => {
   const [user, setUser] = useContext(UserValue);
-  return <SignedOutStack />;
+
   return (
     <>
       {user.data ? (
-        <>{user.id ? <SignedInStack /> : <SignedOutStack />}</>
+        <>
+          {user.id ? (
+            <Box flex={1} safeAreaTop>
+              <SignedInStack />
+            </Box>
+          ) : (
+            <SignedOutStack />
+          )}
+        </>
       ) : (
         <>
           <Splash />
