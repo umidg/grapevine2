9b9b87e02cd7c7d47ea49102a5303e725468f3a5
Umid <umidghimire11@gmail.com>
2022-06-17 20:42:51 +0545

    temping pass

--- src/AtomComponents/Input/index.js
+++ src/AtomComponents/Input/index.js
@@ -1,12 +1,12 @@
-import Input from "./Input";
-import InputCode from "./InputCode";
-import InputIntrests from "./InputIntrests";
-import InputNumber from "./InputNumber";
-import InputText from "./InputText";
-import InputUsername from "./InputUsername";
-import Search from "./Search";
+import Input from './Input';
+import InputCode from './InputCode';
+import InputIntrests from './InputIntrests';
+import InputNumber from './InputNumber';
+import InputText from './InputText';
+import InputUsername from './InputUsername';
+import Search from './Search';
 // import InputPassword from "./InputPassword";
-import InputPassword from "./inputPassword";
+import InputPassword from './InputPassword';
 export {
   Input,
   InputCode,
