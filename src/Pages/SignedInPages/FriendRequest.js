bec4c9375a6cc88077fab421d0d989fd79057169
bipul55 <55142021+bipul55@users.noreply.github.com>
2022-06-24 18:40:08 +0545

    friend request ui

--- src/Pages/SignedInPages/FriendRequest.js
+++ src/Pages/SignedInPages/FriendRequest.js
@@ -4,6 +4,7 @@ import { ScrollView } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { grapevineBackend } from "../../API";
 import { UserValue } from "../../Context/UserContext";
+import Toast from "react-native-root-toast";
 
 import {
   AtomComponents,
@@ -13,29 +14,27 @@ import {
 } from "../../Exports/index";
 const data = [1, 3, 4, 6];
 const FriendRequest = ({ navigation }) => {
-  const { Box1, Notification } = MolecularComponents;
+  const { Box1 } = MolecularComponents;
   const { Search, RoundImage } = AtomComponents;
   const { LoadingMessageModal } = Modal;
-  const { SignInLayout } = Layout;
+  const { SignInLayout, BackLayout } = Layout;
 
   const [friendRequest, setFriendRequest] = useState([]);
   const [user, setUser] = useContext(UserValue);
-  const [showModal, setShowModal] = useState(false);
-  const [modalMessage, setModalMessage] = useState("");
+
   const acceptRequest = (uuid) => {
-    setShowModal(true);
     grapevineBackend(
       "/friendship/acceptfriendrequest",
       { friendship_uuid: uuid, user_accept: user.uuid },
       "POST"
     )
       .then(async ({ data }) => {
-        setModalMessage(data.message);
         const updatedFriends = user?.friends.map((friend, i) => {
           if (friend.uuid === data.data.uuid) {
             let f = friend;
             f.accepted = true;
             setFriend({ ...f });
+
             return f;
           } else {
             return friend;
@@ -45,9 +44,14 @@ const FriendRequest = ({ navigation }) => {
           ...user,
           friends: [...updatedFriends],
         });
+        Toast.show("Accepted Friend Request", {
+          duration: Toast.durations.SHORT,
+        });
       })
       .catch((err) => {
-        setModalMessage("Something Went Wrong");
+        Toast.show("Something Went Wrong", {
+          duration: Toast.durations.SHORT,
+        });
         console.log(err);
       });
   };
@@ -69,101 +73,74 @@ const FriendRequest = ({ navigation }) => {
         })
         .catch((err) => console.log(err));
     });
+    return unsubscribe;
   }, []);
   return (
-    <SignInLayout>
-      <Box w="100%" h="100%" alignItems={"center"}>
-        <Flex
-          direction="row"
-          justifyContent={"center"}
-          alignItems="center"
-          w="100%"
-          mt={5}
-          mb={5}
-        >
-          <LoadingMessageModal
-            showModal={showModal}
-            setShowModal={setShowModal}
-            title="Friend Request"
-            message={modalMessage}
-            setMessage={setModalMessage}
-          />
-          <View
-            position={"absolute"}
-            left={5}
-            h="100%"
-            justifyContent={"center"}
-          >
-            <Pressable onPress={() => navigation.goBack()}>
-              <AntDesign
-                name="arrowleft"
-                size={30}
-                color="black"
-                onPress={() => navigation.pop()}
-              />
-            </Pressable>
+    <BackLayout navigation={navigation} color="#000" safeArea>
+      <Box w="100%" h="100%" alignItems={"center"} bg="#fff">
+        <Text fontWeight="800" fontSize={16} textAlign="center" mb="5">
+          Connection Requests
+        </Text>
+
+        <SignInLayout>
+          <View px={10}>
+            <Search />
           </View>
-          <Text fontWeight={"800"} color="#000" fontSize={22}>
-            Connection Requests
-          </Text>
-        </Flex>
-        <View w="80%">
-          <Search />
-        </View>
-        <View mt={5} w="90%">
-          {friendRequest.length > 0 ? (
-            friendRequest.map((friend_request) => {
-              console.log(friend_request, "friend requests");
-              return (
-                <Box key={friend_request.uuid} px={5}>
-                  <Flex
-                    direction="row"
-                    justifyContent={"space-between"}
-                    alignItems="center"
-                  >
+          <View mt={5} w="90%">
+            {friendRequest.length > 0 ? (
+              friendRequest.map((friend_request) => {
+                console.log(friend_request, "friend requests");
+                return (
+                  <Box key={friend_request.uuid} px={5}>
                     <Flex
                       direction="row"
-                      justifyContent={"flex-start"}
+                      justifyContent={"space-between"}
                       alignItems="center"
                     >
-                      <RoundImage
-                        image={require("../../../assets/Images/1.png")}
-                        size={8}
-                      />
-                      <Text fontSize={18} fontWeight="600" ml={2}>
-                        {"@" + friend_request.username}
-                      </Text>
+                      <Flex
+                        direction="row"
+                        justifyContent={"flex-start"}
+                        alignItems="center"
+                      >
+                        <RoundImage
+                          image={require("../../../assets/Images/1.png")}
+                          size={8}
+                        />
+                        <Text fontSize={18} fontWeight="600" ml={2}>
+                          {"@" + friend_request.username}
+                        </Text>
+                      </Flex>
+                      <Button
+                        onPress={() => acceptRequest(friend_request.uuid)}
+                        h="7"
+                        pt="0"
+                        pb="0"
+                        bg="primary"
+                      >
+                        Accept
+                      </Button>
                     </Flex>
-                    <Button
-                      onPress={() => acceptRequest(friend_request.uuid)}
-                      h="7"
-                      pt="0"
-                      pb="0"
-                      bg="primary"
-                    >
-                      Accept
-                    </Button>
-                  </Flex>
-                </Box>
-              );
-            })
-          ) : (
-            <Text textAlign={"center"}>No friendRequest</Text>
-          )}
-        </View>
-        <View w="100%" mt={5}>
-          <Text fontWeight={"800"} fontSize="32" textAlign={"left"} ml={3}>
-            Suggested
-          </Text>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            {data.map((d) => (
-              //   <Box1 key={d} />
-              <Box1 key={d} />
-            ))}
-          </ScrollView>
-        </View>
+                  </Box>
+                );
+              })
+            ) : (
+              <Text textAlign={"center"}>No friendRequest</Text>
+            )}
+          </View>
+          <View w="100%" mt={5}>
+            <Text fontWeight={"800"} fontSize="32" textAlign={"left"} ml={3}>
+              Suggested
+            </Text>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {data.map((d) => (
+                //   <Box1 key={d} />
+                <Box1 key={d} />
+              ))}
+            </ScrollView>
+          </View>
+        </SignInLayout>
       </Box>
-    </SignInLayout>
+    </BackLayout>
   );
 };
 
--- src/Pages/SignedInPages/Notification.js
+++ src/Pages/SignedInPages/Notification.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { Box, Flex, Text, Pressable, Center, Image } from 'native-base';
-import { AntDesign } from '@expo/vector-icons';
-import { grapevineBackend } from '../../API';
-import { UserValue } from '../../Context/UserContext';
+import React, { useEffect, useState, useContext } from "react";
+import { Box, Flex, Text, Pressable, Center, Image } from "native-base";
+import { AntDesign } from "@expo/vector-icons";
+import { grapevineBackend } from "../../API";
+import { UserValue } from "../../Context/UserContext";
 
-import { MolecularComponents, Layout } from '../../Exports/index';
+import { MolecularComponents, Layout } from "../../Exports/index";
 const NotificationPage = ({ navigation }) => {
   const { Notification } = MolecularComponents;
   const { SignInLayout, BackLayout } = Layout;
@@ -13,44 +13,44 @@ const NotificationPage = ({ navigation }) => {
   const [notifications, setnotification] = useState([]);
 
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
-      grapevineBackend('/notification/get', {}, 'POST')
+    const unsubscribe = navigation.addListener("focus", () => {
+      grapevineBackend("/notification/get", {}, "POST")
         .then(async ({ data }) => {
           if (data.code == 200) {
             setnotification([...data.data.result]);
           } else {
-            console.log('Err: Friend Request', data);
+            console.log("Err: Friend Request", data);
           }
         })
-        .catch((err) => console.log('err', err.message));
+        .catch((err) => console.log("err", err.message));
     });
     return unsubscribe;
   }, []);
 
   return (
-    <BackLayout navigation={navigation} color='#000' safeArea>
-      <Box h='100%' w='100%' bg='white'>
-        <Text fontWeight='800' fontSize={16} textAlign='center' mb='5'>
+    <BackLayout navigation={navigation} color="#000" safeArea>
+      <Box h="100%" w="100%" bg="white">
+        <Text fontWeight="800" fontSize={16} textAlign="center" mb="5">
           Notifications
         </Text>
-        <Pressable onPress={() => navigation.navigate('FriendRequest')}>
+        <Pressable onPress={() => navigation.navigate("FriendRequest")}>
           <Flex
-            flexDirection={'row'}
-            justifyContent='space-between'
-            alignItems={'center'}
-            bg='#3d368218'
+            flexDirection={"row"}
+            justifyContent="space-between"
+            alignItems={"center"}
+            bg="#3d368218"
             py={3}
             px={5}
           >
-            <Text fontSize='16' fontWeight='800' color='#000'>
+            <Text fontSize="16" fontWeight="800" color="#000">
               Connection Requests
             </Text>
 
-            <AntDesign name='caretright' size={12} color='#000' />
+            <AntDesign name="caretright" size={12} color="#000" />
           </Flex>
         </Pressable>
         <SignInLayout>
-          <Box h='100%' w='100%' p='2'>
+          <Box h="100%" w="100%" p="2">
             {/* <Box h='full' w='full'> */}
             {notifications?.length > 0 ? (
               notifications.map((notification) => {
@@ -63,15 +63,15 @@ const NotificationPage = ({ navigation }) => {
                 );
               })
             ) : (
-              <Center h='100%' w='100%'>
+              <Center h="100%" w="100%">
                 <Image
-                  source={require('../../../assets/Logo/Logo.png')}
+                  source={require("../../../assets/Logo/Logo.png")}
                   size={100}
-                  resizeMode='contain'
-                  p='5'
-                  alt='Image'
+                  resizeMode="contain"
+                  p="5"
+                  alt="Image"
                 />
-                <Text fontSize='16' fontWidth='800' color='primary' mt='10'>
+                <Text fontSize="16" fontWidth="800" color="primary" mt="10">
                   Sorry, no notifications.
                 </Text>
               </Center>
