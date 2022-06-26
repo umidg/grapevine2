7061b751aaccb39c2fcba77fd19c9f27d87179b4
Umid <umidghimire11@gmail.com>
2022-06-26 11:54:13 +0545

    ui/ux for comment

--- src/AtomComponents/Image/RoundImage.js
+++ src/AtomComponents/Image/RoundImage.js
@@ -1,29 +1,33 @@
 import React, { memo } from 'react';
 import { Image, Box, Avatar } from 'native-base';
-const RoundImage = ({ size, image, ...props }) => {
+const RoundImage = ({ size, image, boxClass, ...props }) => {
   return (
-    <Box>
-      <Box borderWidth='2' borderColor='primary' p='0.5' borderRadius='full'>
-        <Image
-          alt='image'
-          // style={{
-          //   width: size ? size : 150,
-          //   height: size ? size : 100,
-          //   borderRadius: size / 2,
-          // }}
-          // source={image}
-          // resizeMethod='resize'
-          // resizeMode='contain'
-          // {...props}
-          borderRadius='full'
-          width={size ? size : 150}
-          height={size ? size : 150}
-          source={image}
-          resizeMethod='resize'
-          resizeMode='contain'
-          {...props}
-        />
-      </Box>
+    <Box
+      borderWidth='2'
+      borderColor='primary'
+      p='0.5'
+      borderRadius='full'
+      {...boxClass}
+    >
+      <Image
+        alt='image'
+        // style={{
+        //   width: size ? size : 150,
+        //   height: size ? size : 100,
+        //   borderRadius: size / 2,
+        // }}
+        // source={image}
+        // resizeMethod='resize'
+        // resizeMode='contain'
+        // {...props}
+        borderRadius='full'
+        width={size ? size : 150}
+        height={size ? size : 150}
+        source={image}
+        resizeMethod='resize'
+        resizeMode='contain'
+        {...props}
+      />
     </Box>
   );
 };
--- src/Pages/SignedInPages/CommentPage.js
+++ src/Pages/SignedInPages/CommentPage.js
@@ -1,9 +1,9 @@
-import { View, Text, Flex, Box, Input, TextArea, Button } from "native-base";
-import React, { useContext, useEffect, useState } from "react";
-import { grapevineBackend } from "../../API";
-import { UserValue } from "../../Context/UserContext";
-import { AtomComponents, Layout } from "../../Exports/index";
-import Toast from "react-native-root-toast";
+import { View, Text, Flex, Box, Input, TextArea, Button } from 'native-base';
+import React, { useContext, useEffect, useState } from 'react';
+import { grapevineBackend } from '../../API';
+import { UserValue } from '../../Context/UserContext';
+import { AtomComponents, Layout } from '../../Exports/index';
+import Toast from 'react-native-root-toast';
 
 const { RoundImage } = AtomComponents;
 const { SignInLayout, BackLayout } = Layout;
@@ -12,7 +12,7 @@ const CommentPage = ({ navigation, route }) => {
   const {
     params: { comments, post_uuid },
   } = route;
-  const [commentText, setCommentText] = useState("");
+  const [commentText, setCommentText] = useState('');
   const [user, setUser] = useContext(UserValue);
   const [_comments, _setComments] = useState(comments);
   const [loading, setLoading] = useState(false);
@@ -21,23 +21,23 @@ const CommentPage = ({ navigation, route }) => {
     if (!loading && commentText.length > 0) {
       setLoading(true);
       grapevineBackend(
-        "/comment/create",
+        '/comment/create',
         {
           user_uuid: user.uuid,
           post_uuid: post_uuid,
           comment_text: commentText,
         },
-        "POST"
+        'POST'
       )
         .then(({ data }) => {
           if (data.status) {
             _setComments([..._comments, data.data]);
             setLoading(false);
-            setCommentText("");
+            setCommentText('');
           }
         })
         .catch((err) => {
-          Toast.show("Something Went Wrong", {
+          Toast.show('Something Went Wrong', {
             duration: Toast.durations.SHORT,
           });
           setLoading(false);
@@ -46,82 +46,113 @@ const CommentPage = ({ navigation, route }) => {
   };
 
   return (
-    <BackLayout navigation={navigation} color="black" safeArea>
-      <Box h="full" w="full" bg="white">
-        <Text fontWeight="800" fontSize={16} textAlign="center" mb="2">
+    <BackLayout navigation={navigation} color='black' safeArea>
+      <Box h='full' w='full' bg='white'>
+        <Text fontWeight='800' fontSize={16} textAlign='center' mb='2'>
           Comments
         </Text>
         <SignInLayout>
-          {_comments.map((comment) => {
-            return (
-              <Flex
-                direction="row"
-                key={comment.uuid}
-                my="10px"
-                w="full"
-                px="2"
-              >
-                <RoundImage
-                  image={require("../../../assets/Images/1.png")}
-                  size={30}
-                />
-                <Box ml="2" flex="1" flexDirection="row" pr="5">
-                  <Text fontSize="sm" fontWeight="700">
-                    {comment.user.username}{" "}
-                  </Text>
-                  <Text fontSize="sm" fontWeight="300" textAlign="left">
-                    {comment.comment_text}
-                  </Text>
-                </Box>
-              </Flex>
-            );
-          })}
+          {_comments.length > 0 ? (
+            _comments.map((comment) => {
+              return (
+                <Flex
+                  direction='row'
+                  key={comment.uuid}
+                  my='10px'
+                  w='full'
+                  px='2'
+                >
+                  <RoundImage
+                    image={require('../../../assets/Images/1.png')}
+                    size={30}
+                  />
+                  <Box ml='2' flex='1' flexDirection='row' pr='5'>
+                    <Text fontSize='sm' fontWeight='700'>
+                      {comment.user.username}{' '}
+                    </Text>
+                    <Text fontSize='sm' fontWeight='300' textAlign='left'>
+                      {comment.comment_text}
+                    </Text>
+                  </Box>
+                </Flex>
+              );
+            })
+          ) : (
+            <Box alignItems='center' m='10'>
+              No comments yet.
+            </Box>
+          )}
 
           <Box
-            w="full"
-            flex="1"
-            flexDirection="column"
-            borderTopWidth="1"
-            p="2"
-            borderColor="gray.200"
+            w='full'
+            display='flex'
+            flexDirection='row'
+            borderTopWidth='1'
+            borderColor='gray.200'
+            alignItems='center'
+            p='1'
           >
+            <RoundImage
+              boxClass={{
+                mt: '5',
+                mr: '1',
+                borderColor: 'white',
+              }}
+              flex='0.2'
+              alignSelf='center'
+              size='36'
+              image={require('../../../assets/Images/1.png')}
+            />
             <TextArea
-              fontSize="sm"
-              fontWeight="400"
-              placeholder="Comment here..."
-              color="black"
-              borderWidth={2}
-              h="16"
-              borderColor="gray.400"
-              borderRadius="xl"
+              autoFocus
+              fontSize='sm'
+              fontWeight='400'
+              placeholder={`Add a comment as ${user.username}`}
+              color='black'
+              borderWidth={1}
+              h='12'
+              flex='1'
+              borderColor='gray.400'
+              borderRadius='3xl'
               value={commentText}
               onChangeText={(text) => setCommentText(text)}
               _focus={{
-                bg: "white",
+                bg: 'white',
               }}
-              mb="1"
+              alignItems='center'
+              m='1'
+              mt='5'
+              InputRightElement={
+                <Button
+                  _text={{
+                    color: 'primary',
+                  }}
+                >
+                  Post
+                </Button>
+              }
             />
-            <Box
-              w="full"
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
+            {/* <Box
+              w='full'
+              display='flex'
+              flexDirection='column'
+              justifyContent='center'
             >
               <Button
-                h="10"
+                h='10'
                 p={0}
                 onPress={comment}
-                bg={loading ? "primary:alpha.50" : "primary"}
+                bg={loading ? 'primary:alpha.50' : 'primary'}
                 _text={{
-                  fontWeight: "bold",
-                  fontSize: "16",
+                  fontWeight: 'bold',
+                  fontSize: '16',
                 }}
-                borderRadius="full"
-                mb="5"
+                borderRadius='full'
+                mb='5'
               >
                 Comment
               </Button>
-            </Box>
+            </Box> */}
           </Box>
         </SignInLayout>
       </Box>
